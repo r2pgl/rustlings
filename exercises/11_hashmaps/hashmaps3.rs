9c043a106b95a22987af413061b015bb6306b274
Richard Pringle II <richard2@thepringle.com>
2024-05-29 19:51:24 -0400

    completed

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -15,7 +15,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -23,6 +22,19 @@ use std::collections::HashMap;
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+fn add(scores: &mut HashMap<String, Team>, name: String, scored: u8, conceded: u8) {
+    if !scores.contains_key(&name) {
+        scores.insert(name, Team{
+            goals_scored: scored,
+            goals_conceded: conceded,
+        });
+    }else {
+        let team = scores.get_mut(&name).unwrap();
+        team.goals_conceded += conceded;
+        team.goals_scored += scored;
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -40,6 +52,8 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded by team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        add(&mut scores, team_1_name, team_1_score, team_2_score);
+        add(&mut scores, team_2_name, team_2_score, team_1_score);
     }
     scores
 }
